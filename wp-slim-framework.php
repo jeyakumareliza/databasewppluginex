39b6bd58d9bc27519a1e11b319ecdc38cfe22283
rhildred <rhildred@gmail.com>
2016-11-22 15:04:24 -0500

    added a little database example

--- wp-slim-framework.php
+++ wp-slim-framework.php
@@ -12,6 +12,7 @@
 require_once 'Slim/Slim.php';
 require_once 'SlimWpOptions.php';
 
+
 \Slim\Slim::registerAutoloader();
 new \Slim\SlimWpOptions();
 
@@ -27,6 +28,13 @@ add_action('init', function () {
     if (strstr($_SERVER['REQUEST_URI'], get_option('slim_base_path','slim/api/'))) {
         $slim = new \Slim\Slim();
         do_action('slim_mapping',$slim);
+        $slim->get('/slim/api/user/:u',function($user){
+            printf("User is %s",$user);            
+        });
+        $slim->get('/slim/api/test', function(){
+            global $wpdb, $table_prefix;
+            echo json_encode($wpdb->get_results( $wpdb->prepare("SELECT * FROM " . $table_prefix . "test", null) ));
+        });
         $slim->run();
         exit;
     }
